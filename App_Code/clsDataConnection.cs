8fc52b0a28250529fb05b96de8fb1869467a9dea
Hussein <59957532+HusseinJ4@users.noreply.github.com>
2023-06-18 10:49:48 +0100

    Update clsDataConnection.cs

--- App_Code/clsDataConnection.cs
+++ App_Code/clsDataConnection.cs
@@ -1,12 +1,3 @@
-/** * * * *
-
-Author: Hussein Jama
-Email: P17178096@mydmu.365.ac.uk
-Date: 10/05/2023
-@author H Jama
-@copyright HJA
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
